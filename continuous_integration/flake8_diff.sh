58f93ec505e1e6f22243e51356a2cb045955b9c9
Loïc Estève <loic.esteve@ymail.com>
2016-01-13 14:13:30 +0100

    Bring flake8_diff.sh up-to-date

--- continuous_integration/flake8_diff.sh
+++ continuous_integration/flake8_diff.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 set -e
 
-# Travis goes the git clone with a limited depth (50 at the time of
+# Travis does the git clone with a limited depth (50 at the time of
 # writing). This may not be enough to find the common ancestor with
 # $REMOTE/master so we unshallow the git checkout
 git fetch --unshallow || echo "Unshallowing the git checkout failed"
@@ -10,23 +11,30 @@ git fetch --unshallow || echo "Unshallowing the git checkout failed"
 # Note: upstream has priority if it exists
 git remote -v
 git remote | grep upstream && REMOTE=upstream || REMOTE=origin
-git fetch -v $REMOTE master:remote_master
+# Make sure that $REMOTE/master is set
+git remote set-branches --add $REMOTE master
+git fetch $REMOTE master
+REMOTE_MASTER_REF="$REMOTE/master"
 
-# Find common ancestor between HEAD and remote_master
-COMMIT=$(git merge-base @ remote_master) || \
-    echo "No common ancestor found for $(git show @ -q) and $(git show remote_master -q)"
+echo -e '\nLast 2 commits:'
+echo '--------------------------------------------------------------------------------'
+git log -2 --pretty=short
+
+# Find common ancestor between HEAD and remotes/$REMOTE/master
+COMMIT=$(git merge-base @ $REMOTE_MASTER_REF) || \
+    echo "No common ancestor found for $(git show @ -q) and $(git show $REMOTE_MASTER_REF -q)"
 
 if [ -z "$COMMIT" ]; then
-    # clean-up created branch
-    git branch -D remote_master
-    exit
+    exit 1
 fi
 
-echo Common ancestor is:
-git show $COMMIT --stat
+echo -e "\nCommon ancestor between HEAD and $REMOTE_MASTER_REF is:"
+echo '--------------------------------------------------------------------------------'
+git show --no-patch $COMMIT
 
-echo Running flake8 on the diff with respect to the common ancestor:
-git diff $COMMIT | flake8 --diff
+echo -e '\nRunning flake8 on the diff in the range'\
+     "$(git rev-parse --short $COMMIT)..$(git rev-parse --short @)" \
+     "($(git rev-list $COMMIT.. | wc -l) commit(s)):"
+echo '--------------------------------------------------------------------------------'
 
-# clean-up created branch
-git branch -D remote_master
+git diff $COMMIT | flake8 --diff && echo -e "No problem detected by flake8\n"
